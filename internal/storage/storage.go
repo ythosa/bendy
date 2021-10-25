d5505e97b91fc43b66b16ec06b13db203c53ef69
ythosa <vasus714@yandex.ru>
2021-10-25 21:58:22 +0300

    feat: storage interface

---
+++ internal/storage/storage.go
@@ -0,0 +1,19 @@
+package storage
+
+import "container/list"
+
+type Index interface {
+	Get() (map[string]list.List, error)
+	Update(index map[string]list.List) error
+}
+
+type Files interface {
+	Get() ([]string, error)
+	Put(filename string) error
+	Delete(filename string) error
+}
+
+type Storage struct {
+	Index
+	Files
+}
