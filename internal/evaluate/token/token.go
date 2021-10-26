0a512edc70c16bd8f6407252db78e80096c8c2fb
ythosa <vasus714@yandex.ru>
2021-10-27 02:52:32 +0300

    feat: evaluation - tokens

---
+++ internal/evaluate/token/token.go
@@ -0,0 +1,28 @@
+package token
+
+// Type is type of token
+type Type string
+
+// Token is type for token which contains type and literal
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+// Names of tokens
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifies
+	WORD = "WORD"
+
+	// operators
+	AND = "&"
+	OR  = "|"
+	NOT = "!"
+
+	// Delimiters
+	LPAREN = "("
+	RPAREN = ")"
+)
